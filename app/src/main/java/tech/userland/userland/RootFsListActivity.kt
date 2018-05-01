b047fe667388e74949fb7ca6a79850ea61f51f9a
MatthewTighe <matthewtighe@londontrustmedia.com>
2018-05-01 15:27:05 -0700

    Prototype RecyclerView adapter for RootFfList and Settings activities.

--- app/src/main/java/tech/userland/userland/RootFsListActivity.kt
+++ app/src/main/java/tech/userland/userland/RootFsListActivity.kt
@@ -2,19 +2,26 @@ package tech.userland.userland
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_root_fs_list.*
 
 class RootFsListActivity : AppCompatActivity() {
+
+    val rootFsList: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root_fs_list)
         setSupportActionBar(toolbar)
+
+        genStubRootFsList()
+
+        root_fs_list.layoutManager = LinearLayoutManager(this)
+        root_fs_list.adapter = ListAdapter(rootFsList, this)
+
 
         /*
         fab.setOnClickListener { view ->
@@ -47,5 +54,11 @@ class RootFsListActivity : AppCompatActivity() {
         val intent = Intent(this, HelpActivity::class.java)
         startActivity(intent)
         return true
+    }
+
+    fun genStubRootFsList() {
+        rootFsList.add("Debian")
+        rootFsList.add("Ubuntu")
+        rootFsList.add("Debian with vnc server")
     }
 }
--- app/src/main/java/tech/userland/userland/SettingsActivity.kt
+++ app/src/main/java/tech/userland/userland/SettingsActivity.kt
@@ -2,10 +2,25 @@ package tech.userland.userland
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+    val settingsList: ArrayList<String> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        genStubSettingsList()
+
+        settings_list.layoutManager = LinearLayoutManager(this)
+        settings_list.adapter = ListAdapter(settingsList, this)
+
+    }
+
+    fun genStubSettingsList() {
+        settingsList.add("test1")
+        settingsList.add("test2")
     }
 }
