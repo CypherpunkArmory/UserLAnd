c2114a66e6034b4ce9bf3c150617e5ff5dc277e4
MatthewTighe <matthewtighe@londontrustmedia.com>
2018-05-01 12:25:33 -0700

    Update help stub.

--- app/src/main/java/tech/userland/userland/RootFsListActivity.kt
+++ app/src/main/java/tech/userland/userland/RootFsListActivity.kt
@@ -16,23 +16,20 @@ class RootFsListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_root_fs_list)
         setSupportActionBar(toolbar)
 
+        /*
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
-
+        */
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> navigateToSettings()
             R.id.action_help -> navigateToHelp()
