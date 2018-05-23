fb222ca4b2e7657715a19ba58511eca7209793b2
MatthewTighe <matthewtighe@londontrustmedia.com>
2018-05-23 12:30:19 -0700

    Move fragment to XML and place it below toolbar properly.

--- app/src/main/java/tech/userland/userland/SettingsActivity.kt
+++ app/src/main/java/tech/userland/userland/SettingsActivity.kt
@@ -3,15 +3,12 @@ package tech.userland.userland
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_settings.*
-import tech.userland.userland.ui.SettingsFragment
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fragmentManager.beginTransaction()
-                .replace(android.R.id.content, SettingsFragment())
-                .commit()
-
+        setContentView(R.layout.activity_settings)
+        setSupportActionBar(toolbar)
     }
 }
--- app/src/main/java/tech/userland/userland/ui/SettingsFragment.kt
+++ app/src/main/java/tech/userland/userland/ui/SettingsFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.preference.PreferenceFragment
 import tech.userland.userland.R
 
-public class SettingsFragment : PreferenceFragment() {
+class SettingsFragment : PreferenceFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
