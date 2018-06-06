4fcd0afa3f3b265b8ac2f512504dfa9f0ffea062
Corbin Champion <corbinlc@gmail.com>
2018-06-06 11:32:05 -0700

    refactor how we start/stop servers and clients and switch to using database data in these routines

---
+++ app/src/main/java/tech/ula/utils/ServerUtility.kt
@@ -0,0 +1,43 @@
+package tech.ula.utils
+
+import android.content.Context
+import tech.ula.database.models.Session
+import tech.ula.utils.*
+
+class ServerUtility(private val context: Context) {
+
+    fun Process.pid(): Long {
+        return this.toString().substringAfter("=").substringBefore(",").toLong()
+    }
+
+    private val fileManager by lazy {
+        FileUtility(context)
+    }
+
+    fun startServer(session: Session): Long {
+        if (session.serviceType == "ssh") {
+            return startSSHServer(session)
+        } else if (session.serviceType == "vnc") {
+            //TODO: support vnc server
+            return 0;
+        } else if (session.serviceType == "xsdl") {
+            //TODO: support xsdl server
+            return 0;
+        }
+        return 0;
+    }
+
+    private fun startSSHServer(session: Session): Long {
+        val targetDirectoryName = session.filesystemId.toString()
+        val command = "../support/execInProot /bin/bash -c /support/startSSHServer.sh"
+        val proc = fileManager.wrapWithBusyboxAndExecute(targetDirectoryName, command)
+        return proc.pid()
+    }
+
+    fun stopService(session: Session) {
+        val targetDirectoryName = session.filesystemId.toString()
+        val command = "/data/user/0/tech.userland.userland/files/support/killProcTree.sh " + session.pid.toString()
+        fileManager.wrapWithBusyboxAndExecute(targetDirectoryName, command)
+    }
+
+}
