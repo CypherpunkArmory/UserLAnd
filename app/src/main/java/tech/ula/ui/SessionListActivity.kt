de8830e4bde613d4bdb80b337c2305d18bdcde04
Corbin Champion <corbinlc@gmail.com>
2018-06-20 15:22:00 -0700

    get rid of bogus import

--- app/src/main/java/tech/ula/ui/SessionListActivity.kt
+++ app/src/main/java/tech/ula/ui/SessionListActivity.kt
@@ -18,7 +18,6 @@ import android.preference.PreferenceManager
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
