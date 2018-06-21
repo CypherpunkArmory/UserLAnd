9ca18af7b6f924e4daa576dab55907d431db2d81
Corbin Champion <corbinlc@gmail.com>
2018-06-21 15:03:32 -0700

    remove unused import

--- app/src/main/java/tech/ula/ui/FilesystemEditActivity.kt
+++ app/src/main/java/tech/ula/ui/FilesystemEditActivity.kt
@@ -1,7 +1,6 @@
 package tech.ula.ui
 
 import android.arch.lifecycle.ViewModelProviders
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
