bc5bab22710b31156433063f752474f72fae992a
MatthewTighe <matthewtighe@londontrustmedia.com>
2018-07-18 14:22:04 -0700

    Add database migration.

--- app/src/androidTest/java/tech/ula/model/MigrationTest.kt
+++ app/src/androidTest/java/tech/ula/model/MigrationTest.kt
@@ -1,17 +1,18 @@
 package tech.ula.model
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.db.framework.FrameworkSQLiteOpenHelperFactory
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.testing.MigrationTestHelper
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Assert.*
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
-import java.util.*
 
 @RunWith(AndroidJUnit4::class)
 class MigrationTest {
@@ -27,16 +28,8 @@ class MigrationTest {
     fun migrate1To2() {
         val db = helper.createDatabase(TEST_DB, 1)
 
-//        val oldFilesystemColumns = "id, name, distributionType, archType, defaultUsername, defaultPassword, location, dateCreated, realRoot"
-//        val oldFilesystemValues = "1, 'firstFs', 'dummy', 'dummy', 'dummy', 'dummy', 'dummy', '${Date(0L)}', 0"
-//
-//        val oldSessionColumns = "id, name, filesystemId, filesystemName, active, username, password, geometry, serviceType, clientType, port, pid, startupScript, runAtDeviceStartup, initialCommand"
-//        val oldSessionValues = "1, 'firstSession', 1, 'firstFs', 0, 'dummy', 'dummy', 'dummy', 'dummy', 'dummy', 0, 0, 'dummy', 0, 'dummy'"
-//
-//        db.execSQL("INSERT INTO filesystem ($oldFilesystemColumns) VALUES ($oldFilesystemValues)")
-//        db.execSQL("INSERT INTO session ($oldSessionColumns) VALUES ($oldSessionValues)")
-
-        db.execSQL("INSERT INTO filesystem (id, name, distributionType, archType, defaultUsername, defaultPassword, location, dateCreated, realRoot) VALUES (1, 'firstFs', 'dummy', 'dummy', 'dummy', 'dummy', 'dummy', '${Date(0L)}', 0)")
+        insertVersion1Filesystem(1, "firstFs", db)
+        insertVersion1Session(1, "firstSession", db)
 
         db.close()
 
@@ -44,22 +37,54 @@ class MigrationTest {
 
         val migratedDb = getMigratedDatabase1To2()
         val fs = migratedDb.filesystemDao().getFilesystemByName("firstFs")
-        val fss = migratedDb.filesystemDao().getAllFilesystems().value
-//        val session = migratedDb.sessionDao().getSessionByName("firstSession")
+        val session = migratedDb.sessionDao().getSessionByName("firstSession")
 
         assertFalse(fs.isDownloaded)
-//        assertFalse(session.isExtracted)
-//        assert(session.lastUpdated == 0L)
+        assertFalse(session.isExtracted)
+        assert(session.lastUpdated == 0L)
     }
 
     private fun getMigratedDatabase1To2(): AppDatabase {
-        val db = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(),
-                AppDatabase::class.java)
-                .allowMainThreadQueries()
+        val db = Room.databaseBuilder(InstrumentationRegistry.getTargetContext(),
+                AppDatabase::class.java, TEST_DB)
                 .addMigrations(Migration1To2())
                 .build()
 
         helper.closeWhenFinished(db)
         return db
+    }
+
+    private fun insertVersion1Filesystem(id: Long, name: String, db: SupportSQLiteDatabase) {
+        val filesystemValues = ContentValues()
+        filesystemValues.put("id", id)
+        filesystemValues.put("name", name)
+        filesystemValues.put("distributionType", "dummy")
+        filesystemValues.put("archType", "dummy")
+        filesystemValues.put("defaultUsername", "dummy")
+        filesystemValues.put("defaultPassword", "dummy")
+        filesystemValues.put("location", "dummy")
+        filesystemValues.put("dateCreated", "dummy")
+        filesystemValues.put("realRoot", 0)
+        db.insert("filesystem", SQLiteDatabase.CONFLICT_REPLACE, filesystemValues)
+    }
+
+    private fun insertVersion1Session(id: Long, name: String, db: SupportSQLiteDatabase) {
+        val sessionValues = ContentValues()
+        sessionValues.put("id", id)
+        sessionValues.put("name", name)
+        sessionValues.put("filesystemId", 1)
+        sessionValues.put("filesystemName", "firstFs")
+        sessionValues.put("active", 0)
+        sessionValues.put("username", "dummy")
+        sessionValues.put("password", "dummy")
+        sessionValues.put("geometry", "dummy")
+        sessionValues.put("serviceType", "dummy")
+        sessionValues.put("clientType", "dummy")
+        sessionValues.put("port", 0)
+        sessionValues.put("pid", 0)
+        sessionValues.put("startupScript", "dummy")
+        sessionValues.put("runAtDeviceStartup", 0)
+        sessionValues.put("initialCommand", "dummy")
+        db.insert("session", SQLiteDatabase.CONFLICT_REPLACE, sessionValues)
     }
 }
--- app/src/main/java/tech/ula/model/AppDatabase.kt
+++ app/src/main/java/tech/ula/model/AppDatabase.kt
@@ -38,39 +38,6 @@ abstract class AppDatabase : RoomDatabase() {
 
 class Migration1To2 : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-//        database.execSQL(
-//                "PRAGMA foreign_keys=off; " +
-//
-//                "BEGIN TRANSACTION; " +
-//
-//                "ALTER TABLE filesystem RENAME TO _filesystem_old " +
-//
-//                "CREATE TABLE filesystem ( " +
-//                "( id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-//                "name TEXT NOT NULL, " +
-//                "distributionType TEXT NOT NULL, " +
-//                "archType TEXT NOT NULL, " +
-//                "defaultUsername TEXT NOT NULL, " +
-//                "defaultPassword TEXT NOT NULL, " +
-//                "location TEXT NOT NULL, " +
-//                "dateCreated INTEGER NOT NULL DEFAULT 0 " +
-//                "realRoot INTEGER NOT NULL " +
-//                ");" +
-//
-//                "INSERT INTO filesystem " +
-//                "(id, name, distributionType, archType, defaultUsername, defaultPassword, location, dateCreated, realRoot " +
-//                "SELECT " +
-//                 "id, name, distributionType, archType, defaultUsername, defaultPassword, location, dateCreated, realRoot " +
-//                "FROM _filesystem_old; " +
-//
-//                "INSERT INTO filesystem " +
-//                "(dateCreated) " +
-//                 "VALUE " +
-//                 "${System.currentTimeMillis()} " +
-//
-//                "COMMIT; " +
-//
-//                "PRAGMA foreign_keys=on;")
         database.execSQL("ALTER TABLE filesystem ADD COLUMN isDownloaded INTEGER NOT NULL DEFAULT 0")
 
         database.execSQL("ALTER TABLE session ADD COLUMN isExtracted INTEGER NOT NULL DEFAULT 0")
