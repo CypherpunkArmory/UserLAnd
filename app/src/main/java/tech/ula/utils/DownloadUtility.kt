543d22c29ee827b9de2352ff10a6e68a72b41b67
MatthewTighe <matthewtighe@londontrustmedia.com>
2018-08-01 17:00:06 -0700

    Add test for exception.

--- app/src/main/java/tech/ula/utils/DownloadUtility.kt
+++ app/src/main/java/tech/ula/utils/DownloadUtility.kt
@@ -8,7 +8,6 @@ import tech.ula.model.entities.Session
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
-import java.net.ConnectException
 import java.util.concurrent.TimeUnit
 
 class DownloadUtility(
@@ -148,7 +147,7 @@ class DownloadUtility(
             }
             reader.close()
             return assetList
-        } catch (err: ConnectException) {
+        } catch (err: Exception) {
             throw object : Exception("Error getting asset list") {}
         }
     }
