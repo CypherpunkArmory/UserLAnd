6ed8c93c34bafd61c60e859380d5e19e5b040795
Thomas Luong <Luongt@me.com>
2018-09-07 11:42:01 -0700

    Change options for longpressing an application

--- app/src/main/java/tech/ula/ui/AppListAdapter.kt
+++ app/src/main/java/tech/ula/ui/AppListAdapter.kt
@@ -2,6 +2,7 @@ package tech.ula.ui
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
