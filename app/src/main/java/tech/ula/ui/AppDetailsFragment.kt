14a4ff32acfa0ab43cd67297204e096b61b82640
MatthewTighe <matthewtighe@londontrustmedia.com>
2018-09-20 13:55:22 -0700

    Adjust some sizing and wrap in a scrollview.

--- app/src/main/java/tech/ula/ui/AppDetailsFragment.kt
+++ app/src/main/java/tech/ula/ui/AppDetailsFragment.kt
@@ -5,8 +5,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import kotlinx.android.synthetic.main.frag_app_details.*
 import tech.ula.R
 import tech.ula.model.entities.App
