8e787405ce6460e45a0cc12615ab5bbae0dc7c4b
Thomas Luong <Luongt@me.com>
2018-09-21 13:52:14 -0700

    Clean up Migrations

--- app/src/main/java/tech/ula/model/repositories/UlaDatabase.kt
+++ app/src/main/java/tech/ula/model/repositories/UlaDatabase.kt
@@ -79,11 +79,7 @@ class Migration2To3 : Migration(2, 3) {
 
         database.execSQL("DROP INDEX index_session_name")
         database.execSQL("ALTER TABLE session ADD COLUMN isAppsSession INTEGER NOT NULL DEFAULT 0")
-    }
-}
 
-class Migration3To4 : Migration(3, 4) {
-    override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE filesystem ADD COLUMN defaultVncPassword TEXT NOT NULL DEFAULT 'userland'")
         database.execSQL("ALTER TABLE session ADD COLUMN vncPassword TEXT NOT NULL DEFAULT 'userland'")
     }
