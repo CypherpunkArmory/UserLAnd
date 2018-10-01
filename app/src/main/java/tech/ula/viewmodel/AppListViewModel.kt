6fae9cf15d9e4fafe77ff7b781d8e689afe12a26
Thomas Luong <Luongt@me.com>
2018-10-01 13:22:23 -0700

    Add database listener for filesystems to AppListViewModel

--- app/src/main/java/tech/ula/viewmodel/AppListViewModel.kt
+++ app/src/main/java/tech/ula/viewmodel/AppListViewModel.kt
@@ -4,14 +4,16 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import kotlinx.coroutines.experimental.launch
+import tech.ula.model.daos.FilesystemDao
 import tech.ula.model.daos.SessionDao
 import tech.ula.model.entities.App
+import tech.ula.model.entities.Filesystem
 import tech.ula.model.entities.Session
 import tech.ula.model.repositories.AppsRepository
 import tech.ula.model.repositories.RefreshStatus
 import tech.ula.utils.zipLiveData
 
-class AppListViewModel(private val appsRepository: AppsRepository, private val sessionDao: SessionDao) : ViewModel() {
+class AppListViewModel(private val appsRepository: AppsRepository, private val sessionDao: SessionDao, private val filesystemDao: FilesystemDao) : ViewModel() {
 
     private val activeSessions: LiveData<List<Session>> by lazy {
         sessionDao.findActiveSessions()
@@ -40,10 +42,14 @@ class AppListViewModel(private val appsRepository: AppsRepository, private val s
     fun getRefreshStatus(): LiveData<RefreshStatus> {
         return appsRepository.getRefreshStatus()
     }
+
+    fun getAllFilesystems(): LiveData<List<Filesystem>> {
+        return filesystemDao.getAllFilesystems()
+    }
 }
 
-class AppListViewModelFactory(private val appsRepository: AppsRepository, private val sessionDao: SessionDao) : ViewModelProvider.NewInstanceFactory() {
+class AppListViewModelFactory(private val appsRepository: AppsRepository, private val sessionDao: SessionDao, private val filesystemDao: FilesystemDao) : ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return AppListViewModel(appsRepository, sessionDao) as T
+        return AppListViewModel(appsRepository, sessionDao, filesystemDao) as T
     }
 }
