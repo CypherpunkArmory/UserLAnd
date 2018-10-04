05baec78075938b654038bc5fcdca2e8cc425a75
Thomas Luong <Luongt@me.com>
2018-10-04 11:00:40 -0700

    Remove password trimming from filesystem credentials page

--- app/src/main/java/tech/ula/ui/FilesystemEditFragment.kt
+++ app/src/main/java/tech/ula/ui/FilesystemEditFragment.kt
@@ -112,7 +112,7 @@ class FilesystemEditFragment : Fragment() {
 
         input_filesystem_password.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
-                filesystem.defaultPassword = p0.toString().trim()
+                filesystem.defaultPassword = p0.toString()
             }
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -120,7 +120,7 @@ class FilesystemEditFragment : Fragment() {
 
         input_filesystem_vncpassword.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
-                filesystem.defaultVncPassword = p0.toString().trim()
+                filesystem.defaultVncPassword = p0.toString()
             }
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
