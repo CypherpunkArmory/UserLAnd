190096ad8671eb4e08bdf30b9b7a8b630c34a9a1
Thomas Luong <Luongt@me.com>
2018-10-22 15:06:25 -0700

    Add navigation to account info fragment when user is logged in

--- app/src/main/java/tech/ula/ui/AccountFragment.kt
+++ app/src/main/java/tech/ula/ui/AccountFragment.kt
@@ -1,6 +1,8 @@
 package tech.ula.ui
 
 import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.StrictMode
 import android.support.v4.app.Fragment
@@ -8,13 +10,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.frag_account.*
+import kotlinx.android.synthetic.main.frag_account_info.*
 import kotlinx.android.synthetic.main.frag_account_login.*
 import kotlinx.android.synthetic.main.frag_account_register.*
 import tech.ula.R
 
 class AccountFragment : Fragment() {
 
+    private val currentUser = "currentUser"
     private lateinit var activityContext: Activity
+    private lateinit var prefs: SharedPreferences
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
@@ -26,13 +31,23 @@ class AccountFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         activityContext = activity!!
-        inflateViewStubWithNewFragment(R.layout.frag_account_login)
     }
 
-    fun inflateViewStubWithNewFragment(newFragmentId: Int) {
-        frag_account_layout.removeAllViews()
+    override fun onStart() {
+        super.onStart()
+        prefs = activityContext.getSharedPreferences("account", Context.MODE_PRIVATE)
+        val currentUser = prefs.getString(currentUser, "") ?: ""
+        if (currentUser.isEmpty()) {
+            inflateViewStubWithNewFragment(R.layout.frag_account_register)
+        } else {
+            inflateViewStubWithNewFragment(R.layout.frag_account_info)
+        }
+    }
 
-        val newView = LayoutInflater.from(activityContext).inflate(newFragmentId, frag_account_layout, false)
+    private fun inflateViewStubWithNewFragment(newFragmentId: Int) {
+        frag_account_layout.removeAllViews()
+        val inflater = LayoutInflater.from(activityContext)
+        val newView = inflater.inflate(newFragmentId, frag_account_layout, false)
         frag_account_layout.addView(newView)
 
         when (newFragmentId) {
@@ -40,12 +55,39 @@ class AccountFragment : Fragment() {
                 activityContext.register_prompt.setOnClickListener {
                     inflateViewStubWithNewFragment(R.layout.frag_account_register)
                 }
+
+                activityContext.btn_login.setOnClickListener {
+                    inflateViewStubWithNewFragment(R.layout.frag_account_info)
+                }
             }
             R.layout.frag_account_register -> {
                 activityContext.login_prompt.setOnClickListener {
                     inflateViewStubWithNewFragment(R.layout.frag_account_login)
+                    setLoggedInUser("Thomas")
+                }
+            }
+            R.layout.frag_account_info -> {
+                activityContext.btn_logout.setOnClickListener {
+                    inflateViewStubWithNewFragment(R.layout.frag_account_login)
+                    logoutCurrentUser()
                 }
             }
         }
+    }
+
+    private fun logoutCurrentUser() {
+        with(prefs.edit()) {
+            putString(currentUser, "")
+            apply()
+        }
+    }
+
+    private fun setLoggedInUser(username: String): Boolean {
+        with(prefs.edit()) {
+            putString(currentUser, username)
+            apply()
+        }
+
+        return true
     }
 }
