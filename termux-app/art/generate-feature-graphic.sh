963af6619d31da5815a824b08f0044ba1d9fdf9b
Thomas Luong <Luongt@me.com>
2018-12-27 17:45:46 -0800

    Remove files unneeded for core functionality such as graphics and CI info

--- termux-app/art/generate-feature-graphic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Generating feature graphics to ~/termux-icons/termux-feature-graphic.png..."
-mkdir -p ~/termux-icons/
-rsvg-convert feature-graphic.svg > ~/termux-icons/feature-graphic.png
