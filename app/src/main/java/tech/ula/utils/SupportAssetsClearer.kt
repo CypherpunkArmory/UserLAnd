4dd18a3aa4dd12483154351e12d9692f3a5b2dad
MatthewTighe <matthewtighe@londontrustmedia.com>
2019-01-17 14:39:31 -0800

    Write tests for clearing files.

--- app/src/main/java/tech/ula/utils/SupportAssetsClearer.kt
+++ app/src/main/java/tech/ula/utils/SupportAssetsClearer.kt
@@ -2,6 +2,7 @@ package tech.ula.utils
 
 import java.io.File
 import java.io.FileNotFoundException
+import java.io.IOException
 import java.lang.Exception
 
 class SupportFileClearer(private val filesDir: File, private val assetDirectoryNames: Set<String>) {
@@ -9,28 +10,31 @@ class SupportFileClearer(private val filesDir: File, private val assetDirectoryN
     fun clearAllSupportAssets() {
         if (!filesDir.exists()) throw FileNotFoundException()
         clearTopLevelAssets(assetDirectoryNames)
-        clearFilesystemAssets()
+        clearFilesystemSupportAssets()
     }
 
+    @Throws(IOException::class)
     private fun clearTopLevelAssets(assetDirectoryNames: Set<String>) {
         for (file in filesDir.listFiles()) {
             if (!file.isDirectory) continue
-            if (assetDirectoryNames.contains(file.name)) {
-                file.deleteRecursively()
-            }
+            if (!assetDirectoryNames.contains(file.name)) continue
+            if (!file.deleteRecursively()) throw IOException()
         }
     }
 
-    private fun clearFilesystemAssets() {
+    @Throws(IOException::class)
+    private fun clearFilesystemSupportAssets() {
         for (file in filesDir.listFiles()) {
-            if (file.name.toIntOrNull() != null) {
-                val supportDirectory = File("${file.absolutePath}/support")
-                for (supportFile in supportDirectory.listFiles()) {
-                    // Exclude directories and hidden files.
-                    if (!supportFile.isDirectory && supportFile.name.first() != '.') {
-                        supportFile.delete()
-                    }
-                }
+            if (!file.isDirectory || file.name.toIntOrNull() == null) continue
+
+            val supportDirectory = File("${file.absolutePath}/support")
+            if (!supportDirectory.exists() || !supportDirectory.isDirectory) continue
+
+            for (supportFile in supportDirectory.listFiles()) {
+                // Exclude directories and hidden files.
+                if (supportFile.isDirectory || supportFile.name.first() == '.') continue
+                // Use deleteRecursively extension to match functionality above
+                if (!supportFile.deleteRecursively()) throw IOException()
             }
         }
     }
---
+++ app/src/test/java/tech/ula/utils/SupportFileClearerTest.kt
@@ -0,0 +1,96 @@
+package tech.ula.utils
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import java.io.File
+import java.io.FileNotFoundException
+
+@RunWith(MockitoJUnitRunner::class)
+class SupportFileClearerTest {
+
+    @get:Rule
+    val tempFolder = TemporaryFolder()
+
+    lateinit var filesDir: File
+    lateinit var supportDir: File
+    lateinit var debianDir: File
+    lateinit var filesystemDir: File
+
+    lateinit var filesystemSupportDir: File
+    lateinit var topLevelSupportAssetFile: File
+    lateinit var topLevelDebianAssetFile: File
+    lateinit var hiddenFilesystemSupportFile: File
+    lateinit var nestedFilesystemAssetFile: File
+    lateinit var randomTopLevelFile: File
+    lateinit var randomTopLevelDir: File
+
+    val filesDirName = "filesDir"
+    val debianDirName = "debian"
+    val supportDirName = "support"
+    val filesystemDirName = "1"
+    val assetDirectoryNames = setOf(debianDirName, supportDirName)
+    val assetName = "asset"
+    val hiddenFileName = ".hidden_file"
+
+    lateinit var supportFileClearer: SupportFileClearer
+
+    @Before
+    fun setup() {
+        createTestFiles()
+
+        supportFileClearer = SupportFileClearer(filesDir, assetDirectoryNames)
+    }
+
+    fun createTestFiles() {
+        randomTopLevelFile = tempFolder.newFile()
+        randomTopLevelDir = tempFolder.newFolder()
+        filesDir = tempFolder.newFolder(filesDirName)
+        debianDir = tempFolder.newFolder(filesDirName, debianDirName)
+        supportDir = tempFolder.newFolder(filesDirName, supportDirName)
+        filesystemDir = tempFolder.newFolder(filesDirName, filesystemDirName)
+        filesystemSupportDir = tempFolder.newFolder(filesDirName, filesystemDirName, supportDirName)
+
+        topLevelSupportAssetFile = File("${supportDir.absolutePath}/$assetName")
+        topLevelSupportAssetFile.createNewFile()
+
+        topLevelDebianAssetFile = File("${debianDir.absolutePath}/$assetName")
+        topLevelDebianAssetFile.createNewFile()
+
+        hiddenFilesystemSupportFile = File("${filesystemSupportDir.absolutePath}/$hiddenFileName")
+        hiddenFilesystemSupportFile.createNewFile()
+        nestedFilesystemAssetFile = File("${filesystemSupportDir.absolutePath}/$assetName")
+        nestedFilesystemAssetFile.createNewFile()
+    }
+
+    @Test(expected = FileNotFoundException::class)
+    fun `Throws FileNotFoundException if files directory does not exist`() {
+        filesDir.deleteRecursively()
+
+        supportFileClearer.clearAllSupportAssets()
+    }
+
+    @Test
+    fun `Clears all assets and leaves filesystem structure intact`() {
+        supportFileClearer.clearAllSupportAssets()
+
+        assertTrue(filesDir.exists())
+        assertTrue(randomTopLevelFile.exists())
+        assertTrue(randomTopLevelDir.exists())
+
+        assertFalse(debianDir.exists())
+        assertFalse(supportDir.exists())
+        assertFalse(topLevelDebianAssetFile.exists())
+        assertFalse(topLevelSupportAssetFile.exists())
+
+        assertTrue(filesystemDir.exists())
+        assertTrue(filesystemSupportDir.exists())
+        assertTrue(hiddenFilesystemSupportFile.exists())
+        assertFalse(nestedFilesystemAssetFile.exists())
+    }
+}
