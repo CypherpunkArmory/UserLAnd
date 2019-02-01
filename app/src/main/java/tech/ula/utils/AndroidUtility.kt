7f315215d4b544a09f6c01f5193297e25982a3c9
Thomas Luong <Luongt@me.com>
2019-01-31 16:23:05 -0800

    Remove unused condition check when clientTypePreference for XSDL is selected

--- app/src/main/java/tech/ula/utils/AndroidUtility.kt
+++ app/src/main/java/tech/ula/utils/AndroidUtility.kt
@@ -167,7 +167,7 @@ class AppsPreferences(private val prefs: SharedPreferences) {
         return when {
             pref.toLowerCase() == "ssh" || (app.supportsCli && !app.supportsGui) -> SshTypePreference
             pref.toLowerCase() == "vnc" || (!app.supportsCli && app.supportsGui) -> VncTypePreference
-            pref.toLowerCase() == "xsdl" || (!app.supportsCli && app.supportsGui) -> XsdlTypePreference
+            pref.toLowerCase() == "xsdl" -> XsdlTypePreference
             else -> PreferenceHasNotBeenSelected
         }
     }
