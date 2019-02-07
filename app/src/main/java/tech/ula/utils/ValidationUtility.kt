b10897f04d4988b3b4301b2da4cacdfa6a4fee4a
Thomas Luong <Luongt@me.com>
2019-02-07 14:27:22 -0800

    Refactor ValidationUtility to be unit testable.  Also now checks username is in a blacklist

--- app/src/main/java/tech/ula/utils/ValidationUtility.kt
+++ app/src/main/java/tech/ula/utils/ValidationUtility.kt
@@ -1,31 +1,86 @@
 package tech.ula.utils
 
+import android.content.res.Resources
+import tech.ula.R
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
-class ValidationUtility() {
+class ValidationUtility(systemResources: Resources) {
 
-    fun isUsernameValid(password: String): Boolean {
-        val pattern: Pattern
-        val matcher: Matcher
+    private val resources = systemResources
 
-        val USERNAME_REGEX = "^[a-zA-Z0-9]*\$"
+    fun validateUsername(username: String): Credential {
+        var usernameIsValid = false
+        var errorMessage = ""
+        val blacklist = resources.getStringArray(R.array.blacklisted_usernames)
 
-        pattern = Pattern.compile(USERNAME_REGEX)
-        matcher = pattern.matcher(password)
+        when {
+            username.isEmpty() -> {
+                errorMessage = resources.getString(R.string.error_empty_field)
+            }
+            !validateUsernameCharacters(username) -> {
+                errorMessage = resources.getString(R.string.error_username_invalid_characters)
+            }
+            blacklist.contains(username) -> {
+                errorMessage = resources.getString(R.string.error_username_in_blacklist)
+                errorMessage = String.format(errorMessage, username)
+            }
+            else ->
+                usernameIsValid = true
+        }
 
-        return matcher.matches()
+        return Credential(usernameIsValid, errorMessage)
     }
 
-    fun isPasswordValid(password: String): Boolean {
+    fun validatePasswords(password: String, vncPassword: String): Credential {
+        var passwordIsValid = false
+        var errorMessage = ""
+
+        when {
+            password.isEmpty() || vncPassword.isEmpty() -> {
+                errorMessage = resources.getString(R.string.error_empty_field)
+            }
+            vncPassword.length > 8 || vncPassword.length < 6 -> {
+                errorMessage = resources.getString(R.string.error_vnc_password_length_incorrect)
+            }
+            !validatePasswordCharacters(password) -> {
+                errorMessage = resources.getString(R.string.error_password_invalid)
+            }
+            !validatePasswordCharacters(vncPassword) -> {
+                errorMessage = resources.getString(R.string.error_vnc_password_invalid)
+            }
+            else ->
+                passwordIsValid = true
+        }
+
+        return Credential(passwordIsValid, errorMessage)
+    }
+
+    private fun validateUsernameCharacters(username: String): Boolean {
+        val usernameRegex = "([a-z_][a-z0-9_]{0,30})"
+
+        val compiledRegex = Pattern.compile(usernameRegex)
+        val matcher = compiledRegex.matcher(username)
+        val hasValidCharacters = matcher.matches()
+
+        if (hasValidCharacters) {
+            return true
+        }
+        return false
+    }
+
+    private fun validatePasswordCharacters(password: String): Boolean {
         val pattern: Pattern
         val matcher: Matcher
 
-        val PASSWORD_REGEX = "^[a-zA-Z0-9!@#$%^&*()_+=,./?<>:]*\$"
+        val passwordRegex = "^[a-zA-Z0-9!@#$%^&*()_+=,./?<>:]*\$"
 
-        pattern = Pattern.compile(PASSWORD_REGEX)
+        pattern = Pattern.compile(passwordRegex)
         matcher = pattern.matcher(password)
 
         return matcher.matches()
     }
 }
+
+sealed class CredentialValidation
+data class Credential(val credentialIsValid: Boolean, val errorMessage: String) : CredentialValidation()
