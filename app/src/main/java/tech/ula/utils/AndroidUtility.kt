69ea5024381f31bbb5ae7bcd73f191d77d3a7cdc
MatthewTighe <matthewtighe@londontrustmedia.com>
2019-02-07 16:24:09 -0800

    Add a comment.

--- app/src/main/java/tech/ula/utils/AndroidUtility.kt
+++ app/src/main/java/tech/ula/utils/AndroidUtility.kt
@@ -58,6 +58,7 @@ fun displayGenericErrorDialog(activity: Activity, titleId: Int, messageId: Int) 
             .create().show()
 }
 
+// Add or change asset types as needed for testing and staggered releases.
 fun getBranchToDownloadAssetsFrom(assetType: String): String {
     return when (assetType) {
         "support" -> "beta"
