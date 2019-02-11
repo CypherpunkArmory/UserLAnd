00cc365fdbfb8869a83422ce0a021a816931ff42
MatthewTighe <matthewtighe@londontrustmedia.com>
2019-02-11 11:46:44 -0800

    Switch support asset fetching back to master branch and rebuild release apk.

--- app/src/main/java/tech/ula/utils/AndroidUtility.kt
+++ app/src/main/java/tech/ula/utils/AndroidUtility.kt
@@ -61,7 +61,7 @@ fun displayGenericErrorDialog(activity: Activity, titleId: Int, messageId: Int) 
 // Add or change asset types as needed for testing and staggered releases.
 fun getBranchToDownloadAssetsFrom(assetType: String): String {
     return when (assetType) {
-        "support" -> "beta"
+        "support" -> "master"
         "apps" -> "master"
         else -> "master"
     }
