2877c326e73cd98f65137b852372afcf70b17a7b
Thomas Luong <Luongt@me.com>
2019-02-12 14:06:40 -0800

    Fix logic when revew request tries to show up, user should not have already dismissed it in order to show up

--- app/src/main/java/tech/ula/MainActivity.kt
+++ app/src/main/java/tech/ula/MainActivity.kt
@@ -175,7 +175,7 @@ class MainActivity : AppCompatActivity(), SessionListFragment.SessionSelection, 
         val minimumTimesOpenedReached = usageUtility.numberOfTimesOpenedIsGreaterThanThreshold()
         val userAlreadyGaveReviewOrDismissed = usageUtility.getUserGaveFeedback()
 
-        if (minimumTimePassedReached && minimumTimesOpenedReached && userAlreadyGaveReviewOrDismissed) {
+        if (minimumTimePassedReached && minimumTimesOpenedReached && !userAlreadyGaveReviewOrDismissed) {
             val requestReviewView = layoutInflater.inflate(R.layout.list_item_review_request, null)
             requestReviewView.visibility = View.VISIBLE
             val viewHolder = findViewById<ViewGroup>(R.id.request_review_insert_point)
