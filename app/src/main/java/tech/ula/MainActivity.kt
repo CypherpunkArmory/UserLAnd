8c20c2ec6dcaadb3998a7630f7e40a36cfd5bd5e
Thomas Luong <Luongt@me.com>
2019-02-22 10:26:23 -0800

    Add missing import

--- app/src/main/java/tech/ula/MainActivity.kt
+++ app/src/main/java/tech/ula/MainActivity.kt
@@ -27,6 +27,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.view.animation.AlphaAnimation
 import android.widget.Button
 import android.widget.RadioButton
