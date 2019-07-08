99af11f7af5778495acc300a2a7e6eebcbce5fa6
MatthewTighe <matthewtighe@londontrustmedia.com>
2019-07-08 14:07:56 -0700

    Fix version check for Q warning.

--- app/src/main/java/tech/ula/utils/QWarningHandler.kt
+++ app/src/main/java/tech/ula/utils/QWarningHandler.kt
@@ -24,7 +24,7 @@ class QWarningHandler(private val prefs: SharedPreferences, private val ulaFiles
     }
 
     private fun versionDoesNotMatchRequirement(): Boolean {
-        return prefs.getString(versionKey, "") ?: "" < "v2.1.5"
+        return prefs.getString(versionKey, "") ?: "" < "v2.6.0"
     }
 
     private fun userHasFilesystems(): Boolean {
