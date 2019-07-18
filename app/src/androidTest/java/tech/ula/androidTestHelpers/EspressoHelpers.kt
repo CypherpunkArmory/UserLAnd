18b225a77b869374d1a561c0b0ed76692bf4b32d
MatthewTighe <matthewtighe@londontrustmedia.com>
2019-07-18 16:25:41 -0700

    Create happy path end-to-end test.

--- app/src/androidTest/java/tech/ula/androidTestHelpers/EspressoHelpers.kt
+++ app/src/androidTest/java/tech/ula/androidTestHelpers/EspressoHelpers.kt
@@ -1,39 +1,52 @@
 package tech.ula.androidTestHelpers
 
 import android.app.Activity
-import android.view.View
+import android.view.WindowManager
 import androidx.annotation.IdRes
+import androidx.annotation.StringRes
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.Root
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.internal.failurehandler.BaristaException
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
+import androidx.test.uiautomator.UiSelector
+import androidx.test.uiautomator.UiDevice
+import android.os.Build
+import androidx.test.platform.app.InstrumentationRegistry
+import android.view.KeyCharacterMap
+import android.view.KeyEvent
+import java.io.File
+import java.util.concurrent.TimeoutException
 
-fun @receiver:IdRes Int.matchView(): ViewInteraction =
-        Espresso.onView(ViewMatchers.withId(this))
-
-
-fun ViewInteraction.checkVisible(): ViewInteraction =
-        check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
-
-fun ViewInteraction.checkInvisible(): ViewInteraction =
-        check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)))
-
-fun @receiver:IdRes Int.checkVisible(): ViewInteraction =
-        this.matchView().checkVisible()
-
-fun @receiver:IdRes Int.checkInvisible(): ViewInteraction =
-        this.matchView().checkInvisible()
-
-fun <T: View> Int.waitForVisibilityChange(activity: Activity) {
-    val view = activity.findViewById<T>(this)
-    val target = if (view.visibility == View.VISIBLE) {
-        View.INVISIBLE
-    } else {
-        View.VISIBLE
+fun waitForDisplay(@IdRes id: Int, timeout: Long = 300_000) {
+    val test = {
+        try {
+            assertDisplayed(id)
+            true
+        } catch (err: BaristaException) {
+            false
+        }
     }
-    while (view.visibility != target) Thread.sleep(1)
+    waitForSuccess(test, timeout)
+}
+
+fun waitForFile(file: File, timeout: Long = 300_000) {
+    val test = { file.exists() }
+    waitForSuccess(test, timeout)
+}
+
+fun waitForSuccess(test: () -> Boolean, timeout: Long) {
+    val startTime = System.currentTimeMillis()
+    while (!test()) {
+        val currTime = System.currentTimeMillis()
+        if (currTime >= startTime + timeout) throw TimeoutException()
+        Thread.sleep(10)
+    }
 }
 
 fun Int.waitForRefresh(activity: Activity) {
@@ -42,3 +55,65 @@ fun Int.waitForRefresh(activity: Activity) {
 }
 
 fun waitForSwipeRefresh(@IdRes id: Int, activity: Activity) = id.waitForRefresh(activity)
+
+class ToastMatcher : TypeSafeMatcher<Root>() {
+    override fun describeTo(description: Description) {
+        description.appendText("is toast")
+    }
+
+    override fun matchesSafely(root: Root): Boolean {
+        val type = root.windowLayoutParams.get().type
+        @Suppress("DEPRECATION") // TYPE_TOAST is deprecated
+        if (type == WindowManager.LayoutParams.TYPE_TOAST || type == WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY) {
+            val windowToken = root.decorView.windowToken
+            val appToken = root.decorView.applicationWindowToken
+            if (windowToken == appToken) {
+                // Window isn't contained by any other windows
+                return true
+            }
+        }
+        return false
+    }
+}
+
+fun @receiver:StringRes Int.matchText(): ViewInteraction =
+        Espresso.onView(ViewMatchers.withText(this))
+
+// TODO this doesn't quite work. will be useful for failure tests
+fun @receiver:StringRes Int.notDisplayedInToast(): ViewInteraction =
+        this.matchText().inRoot(ToastMatcher()).check(ViewAssertions.doesNotExist())
+
+fun allowPermissions() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        Thread.sleep(3000)
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        val allowPermissions = device.findObject(UiSelector()
+                .clickable(true)
+                .checkable(false)
+                .text("Allow"))
+        if (allowPermissions.exists()) {
+            allowPermissions.click()
+        }
+    }
+}
+
+fun String.enterText() {
+    val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    val characterMap = KeyCharacterMap.load(KeyCharacterMap.VIRTUAL_KEYBOARD)
+    val events = characterMap.getEvents(this.toCharArray())
+    for (event in events) {
+        // There are ACTION_UP and ACTION_DOWN events generated. Only need to press keycode once
+        if (event.action == KeyEvent.ACTION_UP) continue
+        device.pressKeyCode(event.keyCode)
+    }
+    device.pressEnter()
+}
+
+fun executeScript(script: String, location: File) {
+    val scriptName = "script.sh"
+    val scriptFile = File(location, scriptName)
+    scriptFile.writeText(script)
+    "chmod 777 $scriptName".enterText()
+    Thread.sleep(500)
+    "bash $scriptName".enterText()
+}
--- app/src/androidTest/java/tech/ula/ui/MainActivityTest.kt
+++ app/src/androidTest/java/tech/ula/ui/MainActivityTest.kt
@@ -1,26 +1,24 @@
 package tech.ula.ui
 
-import android.Manifest
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.schibsted.spain.barista.assertion.BaristaListAssertions.assertDisplayedAtPosition
-import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertContains
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.schibsted.spain.barista.interaction.BaristaDialogInteractions.clickDialogPositiveButton
+import com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo
 import com.schibsted.spain.barista.interaction.BaristaListInteractions.clickListItem
-import com.schibsted.spain.barista.interaction.PermissionGranter
+import com.schibsted.spain.barista.interaction.BaristaRadioButtonInteractions.clickRadioButtonItem
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import tech.ula.MainActivity
 import tech.ula.R
-import tech.ula.androidTestHelpers.checkVisible
-import tech.ula.androidTestHelpers.waitForRefresh
-import tech.ula.androidTestHelpers.waitForSwipeRefresh
+import tech.ula.androidTestHelpers.* // ktlint-disable no-wildcard-imports
+import java.io.File
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -30,21 +28,91 @@ class MainActivityTest {
     val activityRule = ActivityTestRule(MainActivity::class.java)
 
     private lateinit var activity: MainActivity
+    private val appName = "Alpine"
+    private val username = "alpine_user"
+    private val sshPassword = "ssh"
+    private val vncPassword = "vncpass"
+    private val homeLocation = "1/home/$username"
+    private lateinit var homeDirectory: File
 
     @Before
     fun setup() {
         activity = activityRule.activity
+        homeDirectory = File(activity.filesDir, homeLocation)
     }
 
     @Test
-    fun test() {
+    fun testHappyPath() {
+        Thread.sleep(1000)
         assertDisplayed(R.id.app_list_fragment)
+
         waitForSwipeRefresh(R.id.swipe_refresh, activity)
-        assertDisplayedAtPosition(R.id.list_apps, 1, R.id.apps_name, "Alpine")
+
+        // Click alpine
+        assertDisplayedAtPosition(R.id.list_apps, 1, R.id.apps_name, appName)
         clickListItem(R.id.list_apps, 1)
+
+        // Handle permissions
         assertContains(R.string.alert_permissions_necessary_title)
         clickDialogPositiveButton()
-        PermissionGranter.allowPermissionsIfNeeded(Manifest.permission.READ_EXTERNAL_STORAGE)
-        PermissionGranter.allowPermissionsIfNeeded(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        allowPermissions()
+
+        // Set filesystem credentials
+        Thread.sleep(1000)
+        assertContains(R.string.filesystem_credentials_reasoning)
+        writeTo(R.id.text_input_username, username)
+        writeTo(R.id.text_input_password, sshPassword)
+        writeTo(R.id.text_input_vnc_password, vncPassword)
+        clickDialogPositiveButton()
+
+        // Set session type to ssh
+        assertContains(R.string.prompt_app_connection_type_preference)
+        clickRadioButtonItem(R.id.radio_apps_service_type_preference, R.id.ssh_radio_button)
+        clickDialogPositiveButton()
+
+        // Wait for progress dialog to complete
+        assertDisplayed(R.id.progress_bar_session_list)
+        waitForDisplay(R.string.progress_fetching_asset_lists)
+        waitForDisplay(R.string.progress_downloading)
+        waitForDisplay(R.string.progress_copying_downloads)
+        waitForDisplay(R.string.progress_verifying_assets)
+        waitForDisplay(R.string.progress_setting_up_filesystem)
+        waitForDisplay(R.string.progress_starting)
+
+        // Enter session and create a file to verify correct access
+        waitForDisplay(R.id.terminal_view)
+        sshPassword.enterText()
+        Thread.sleep(500)
+        "touch test.txt".enterText()
+        Thread.sleep(500)
+        val expectedFile = File(homeDirectory, "test.txt")
+        assertTrue(expectedFile.exists())
+
+        val expectedStatusFiles = doHappyPathTestScript()
+        Thread.sleep(500)
+        for (file in expectedStatusFiles) {
+            waitForFile(file, timeout = 60_000)
+        }
+    }
+
+    private fun doHappyPathTestScript(): List<File> {
+        val startedFile = File(homeDirectory, "test.scriptStarted")
+        val updatedFile = File(homeDirectory, "test.apkUpdated")
+        val addedFile = File(homeDirectory, "test.pkgAdded")
+        val indexFile = File(homeDirectory, "test.index")
+        val finishedFile = File(homeDirectory, "test.finished")
+        val script =
+                """
+            touch ${startedFile.name}
+            sudo apk update
+            touch ${updatedFile.name}
+            sudo apk add curl
+            touch ${addedFile.name}
+            curl google.com > ${indexFile.name}
+            touch ${finishedFile.name}
+            """.trimIndent()
+
+        executeScript(script, homeDirectory)
+        return listOf(startedFile, updatedFile, addedFile, indexFile, finishedFile)
     }
 }
