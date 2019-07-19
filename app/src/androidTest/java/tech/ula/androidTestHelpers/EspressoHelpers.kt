d1b67b0326bf91384eafc163aad500b0c54e38f4
MatthewTighe <matthewtighe@londontrustmedia.com>
2019-07-19 14:38:39 -0700

    Remove unused import.

--- app/src/androidTest/java/tech/ula/androidTestHelpers/EspressoHelpers.kt
+++ app/src/androidTest/java/tech/ula/androidTestHelpers/EspressoHelpers.kt
@@ -1,7 +1,6 @@
 package tech.ula.androidTestHelpers
 
 import android.app.Activity
-import android.app.Instrumentation
 import android.content.Context
 import android.content.pm.PackageManager
 import android.view.WindowManager
