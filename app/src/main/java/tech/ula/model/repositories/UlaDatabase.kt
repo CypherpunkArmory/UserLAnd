5a1d62b50256bcb07225584e702cb5d6c9e3dc8e
Corbin Champion <corbinlc@gmail.com>
2019-12-18 16:26:31 -0800

    fix recent database change

--- app/src/main/java/tech/ula/model/repositories/UlaDatabase.kt
+++ app/src/main/java/tech/ula/model/repositories/UlaDatabase.kt
@@ -154,7 +154,7 @@ class Migration7To8 : Migration(7, 8) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE session ADD COLUMN serviceLocation TEXT NOT NULL DEFAULT 'local'")
         database.execSQL("ALTER TABLE session ADD COLUMN ip TEXT NOT NULL DEFAULT ''")
-        database.execSQL("ALTER TABLE session ADD COLUMN filesystemName TEXT NOT NULL DEFAULT ''")
+        database.execSQL("ALTER TABLE session ADD COLUMN filesystemType TEXT NOT NULL DEFAULT ''")
 
         database.execSQL("ALTER TABLE apps ADD COLUMN supportsLocal INTEGER NOT NULL DEFAULT 1")
         database.execSQL("ALTER TABLE apps ADD COLUMN supportsRemote INTEGER NOT NULL DEFAULT 0")
