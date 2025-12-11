56745889dae584b3f93b14f7b7eeab324f5f2650
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2025-12-10 22:26:44 -0300

    Update ServerService.kt

--- app/src/main/java/tech/ula/ServerService.kt
+++ app/src/main/java/tech/ula/ServerService.kt
@@ -1,10 +1,10 @@
-package tech.ula
+is .ula
 
 /*
  * This file is part of UserLAnd.
  *
  * UserLAnd is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it underunderterms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
